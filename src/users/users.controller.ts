20c514ed09b3def6f19c8873ee36dc7f0edfe70f
Valentin Fourny <fourny.valentin@gmail.com>
2025-01-08 15:48:10 +0100

    feat: add jwt course

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,5 @@
-import { PrismaClient } from '@prisma/client';
 import { Request, Response } from 'express';
-
-const prisma = new PrismaClient();
+import prisma from '../client';
 
 export const getUsers = async (_req: Request, res: Response) => {
   const users = await prisma.user.findMany({ include: { posts: true } });
